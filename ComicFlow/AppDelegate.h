3b894dee101de96714041fdcf1fbbe067c91062c
huangkun <huangkun@vipkid.com.cn>
2018-06-16 18:53:32 +0800

    Initial Commit

---
+++ ComicFlow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ComicFlow
+//
+//  Created by huangkun on 2018/6/16.
+//  Copyright © 2018年 vipkid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ComicFlow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ComicFlow
+//
+//  Created by huangkun on 2018/6/16.
+//  Copyright © 2018年 vipkid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
